b8ea9190f08702dd96f8f0661a4b03690a8eaad8
Isaac Mann <isaacplmann+git@gmail.com>
2019-07-12 16:13:39 -0400

    feat: Storybook schematics

---
+++ packages/storybook/src/utils/versions.ts
@@ -0,0 +1,5 @@
+export const nxVersion = '*';
+export const storybookAngularVersion = '5.1.9';
+export const storybookAddonKnobsVersion = '5.1.9';
+export const babelCoreVersion = '7.5.4';
+export const babelLoaderVersion = '8.0.6';
